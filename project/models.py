3345f994fabcc752a9d915baa6c55edb233af4ce
hrshl13 <harshal26092002@gmail.com>
2022-08-19 13:25:08 +0530

    Models Done

--- project/models.py
+++ project/models.py
@@ -1,3 +1,19 @@
+import string
 from django.db import models
 
 # Create your models here.
+class StudentModel(models.Model):
+    name = models.CharField(max_length=100)
+    rollNum = models.IntegerField(primary_key=True)
+    def __str__(self) -> str:
+        string = f"{self.name}({self.rollNum})"
+        return string
+    
+
+class AttendanceModel(models.Model):
+    student = models.ForeignKey(to=StudentModel)
+    datetime = models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        string = f"{self.student.rollNum}({self.datetime})"
+        return string
+
