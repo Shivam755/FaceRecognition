599ed385a28865ce3b9b4062baa952682d0e8260
Shivam Gupta <66762938+Shivam755@users.noreply.github.com>
2022-08-19 13:50:46 +0530

    solving a small issue

--- project/models.py
+++ project/models.py
@@ -1,19 +1,21 @@
 import string
 from django.db import models
 
+
 # Create your models here.
 class StudentModel(models.Model):
     name = models.CharField(max_length=100)
     rollNum = models.IntegerField(primary_key=True)
+
     def __str__(self) -> str:
         string = f"{self.name}({self.rollNum})"
         return string
-    
+
 
 class AttendanceModel(models.Model):
-    student = models.ForeignKey(to=StudentModel)
+    student = models.ForeignKey(to=StudentModel, on_delete=models.CASCADE)
     datetime = models.DateTimeField(auto_now=True)
+
     def __str__(self) -> str:
         string = f"{self.student.rollNum}({self.datetime})"
         return string
-
